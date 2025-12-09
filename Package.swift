0865f0e1a2d125d1febc1684d21b1124035cc94a
kenneth.ljh <see2851@gmail.com>
2025-12-09 14:25:30 +0800

    feat: Update binaryTarget URL and checksum
    Updated the URL to point to the GitHub release artifact and revised the checksum to match the new zip file.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AlicloudELS",
-            url: "https://ios-repo.oss-cn-shanghai.aliyuncs.com/els/1.0.3/els.zip",
-            checksum: "5ee188d7e9db610c0033372de8233b4de4b82cebea6df5dec9d65eb2f6b62c48"
+            url: "https://github.com/KennethLaw/AlicloudELS/releases/download/1.0.3/AlicloudELS.xcframework.zip",
+            checksum: "3ee413447d9c50d2a2685b576c6001466621eadaba1e6e575542068db3e98b13"
         )
     ]
 )
